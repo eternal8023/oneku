f88797268c9f7913f0a82f4d53855814664b88c0
wuai <873636145@qq.com>
2021-06-10 21:04:15 +0800

    最最最后一版

--- dist/js/xiangqing.js
+++ dist/js/xiangqing.js
@@ -1,4 +1,42 @@
-var wode=document.querySelector(".head-r"),box=document.querySelector(".concentt"),name1=getCookie("name");wode.onclick=function(t){"."==((t=t||window.event).target||t.srcElement).innerHTML&&(name1?location.href="../html/gouwuche.html":(alert("尚未登录，请登录"),t=location.href,location.href="./denglu.html?newUrl="+t))};var search1=location.search,dt,ar1,id;search1?(ar1=search1.split("="),"?id"==ar1[0]?(id=ar1[1],async function(){dt=await promiseAjax({url:"../php/xiangqing.php",data:"id="+id}),dt=eval("("+dt+")");var str=`
+var wode=document.querySelector('.head-r')
+var box=document.querySelector(".concentt")
+var name1=getCookie("name")
+wode.onclick=function(e){
+    var e = e || window.event
+    //目标对象兼容
+    var target=e.target || e.srcElement
+    if(target.innerHTML=="."){
+        //判断姓名是否存在
+    if(name1){
+        location.href='../html/gouwuche.html'
+    }else{
+    alert("尚未登录，请登录")
+    //获取当前地址栏信息
+    var url=location.href
+    location.href='./denglu.html?newUrl='+url
+}
+}}
+//获取地址栏中的参数信息
+var search1=location.search
+var dt
+//判断当前地址栏中是否有参数
+if(search1){
+    //分割字符串
+    var ar1=search1.split("=")
+    //判断当前参数是否为id
+    if(ar1[0]=="?id"){
+        //获取当前参数的值
+        var id=ar1[1];
+        (async function(){
+            //发送请求，并获取响应结果
+            dt=await promiseAjax({
+                url:'../php/xiangqing.php',
+                data:'id='+id
+            })
+            //把字符串转为对象
+            dt=eval('('+dt+')')
+            //把字符渲染到页面当中
+            var str=`
             <div class="concent">
             <div class="con-c">
                 <div class="conc-t">
@@ -103,4 +141,152 @@ var wode=document.querySelector(".head-r"),box=document.querySelector(".concentt
                 </div>
             </div>
         </div>
-        `;box.innerHTML=str}()):(alert("参数有误"),location.href="./list.html")):(alert("非法进入，请选择商品"),location.href="./list.html"),box.onclick=function(e){var e=e||window.event,target=e.target||e.srcElement,cartList4,bool,cartList4,bool;"加入购物车"==target.innerHTML&&(cartList4=localStorage.getItem("cartList4")||[],0<cartList4.length?(cartList4=eval("("+cartList4+")"),bool=!0,cartList4.forEach(t=>{dt.id==t.id&&(bool=!1,t.hai_number++,localStorage.setItem("cartList4",JSON.stringify(cartList4)))}),bool&&(dt.hai_number=1,cartList4.push(dt),localStorage.setItem("cartList4",JSON.stringify(cartList4)))):(dt.hai_number=1,cartList4.push(dt),localStorage.setItem("cartList4",JSON.stringify(cartList4)))),"立即购买"==target.innerHTML&&(cartList4=localStorage.getItem("cartList4")||[],0<cartList4.length?(cartList4=eval("("+cartList4+")"),bool=!0,cartList4.forEach(t=>{dt.id==t.id&&(bool=!1,t.hai_number++,localStorage.setItem("cartList4",JSON.stringify(cartList4)))}),bool&&(dt.hai_number=1,cartList4.push(dt),localStorage.setItem("cartList4",JSON.stringify(cartList4))),location.href="../html/gouwuche.html"):(dt.hai_number=1,cartList4.push(dt),localStorage.setItem("cartList4",JSON.stringify(cartList4))))};
+        `
+        box.innerHTML=str
+var leftBox=document.querySelector(".concentt .concp-l")
+var mark=document.querySelector(".concentt .mark")
+var rightBox=document.querySelector(".concentt .rightBox")
+    //给左边大盒子对象绑定事件
+leftBox.onmouseover=function(){
+    //显示隐藏的盒子
+    mark.style.display='block'
+    rightBox.style.display='block'
+}
+leftBox.onmouseout=function(){
+    //隐藏指定盒子
+    mark.style.display='none'
+    rightBox.style.display='none'
+}
+leftBox.onmousemove=function(e){
+    //兼容事件对象
+    var e = e || window.event
+    //获取移动距离
+    var left1=e.pageX-leftBox.offsetLeft-parseInt(mark.offsetWidth)
+    var top1=e.pageY-leftBox.offsetTop-parseInt(mark.offsetHeight)
+    //设置边界条件
+    var maxX=leftBox.offsetWidth-mark.offsetWidth
+    var maxY=leftBox.offsetHeight-mark.offsetHeight
+    //右边图片移动的距离
+    var imgLeft,imgTop
+    //水平方向的判断
+    if(left1<=0){
+        mark.style.left="0px"
+        imgLeft=0
+    }else if(left1>=maxX){
+        mark.style.left=maxX+'px'
+        imgLeft=maxX
+    }else{
+        mark.style.left=left1+'px'
+        imgLeft=left1
+    }
+
+    //垂直方向
+    if(top1<=0){
+        mark.style.top="0px"
+        imgTop=0
+    }else if(top1>=maxY){
+        mark.style.top=maxY+'px'
+        imgTop=maxY
+    }else{
+        mark.style.top=top1+'px'
+        imgTop=top1
+    }
+
+    //获取右边盒子中的图片
+    var img=rightBox.querySelector("img")
+    //给右边图片设置偏移量
+    img.style.left=-2.5*imgLeft+'px'
+    img.style.top=-2.5*imgTop+'px'
+}
+})()
+    }else{
+        alert("参数有误")
+        location.href="./list.html"
+    }
+}else{
+    alert("非法进入，请选择商品")
+    location.href="./list.html"
+}
+
+//给大盒子对象绑定点击事件
+box.onclick=function(e){
+    //事件对象兼容
+    var e = e || window.event
+    //事件目标的兼容
+    var target=e.target || e.srcElement
+    //判断当前点击的是否为加入购物车
+    if(target.innerHTML=="加入购物车"){
+        //获取localStorage中cartList4
+        var cartList4=localStorage.getItem("cartList4")||[]
+        //判断当前cartList4是否存在
+        if(cartList4.length>0){
+            //把cartList4转为数组对象
+            cartList4=eval('('+cartList4+')')
+            var bool=true //是否有相同的商品
+            //遍历数组
+            cartList4.forEach(item=>{
+                //判断当前遍历的商品是否跟添加的商品相同
+                if(dt.id==item.id){
+                    bool=false
+                    //让当前的商品数量加1
+                    item.hai_number++
+                    //重新给localStorage设置键值对
+                    localStorage.setItem("cartList4",JSON.stringify(cartList4))
+                }
+            })
+            //判断bool是否为true
+            if(bool){
+                 //修改dt对象中的数量
+                dt.hai_number=1
+                //把当前商品追加到cartList4中
+                cartList4.push(dt)
+                //重新给localStorage设置键值对
+                localStorage.setItem("cartList4",JSON.stringify(cartList4))
+            }
+        }else{
+            //修改dt对象中的数量
+            dt.hai_number=1
+            //把当前商品追加到cartList4中
+            cartList4.push(dt)
+            //重新给localStorage设置键值对
+            localStorage.setItem("cartList4",JSON.stringify(cartList4))
+        }
+    }
+    if(target.innerHTML=="立即购买"){
+        var cartList4=localStorage.getItem("cartList4")||[]
+        //判断当前cartList4是否存在
+        if(cartList4.length>0){
+            //把cartList4转为数组对象
+            cartList4=eval('('+cartList4+')')
+            var bool=true //是否有相同的商品
+            //遍历数组
+            cartList4.forEach(item=>{
+                //判断当前遍历的商品是否跟添加的商品相同
+                if(dt.id==item.id){
+                    bool=false
+                    //让当前的商品数量加1
+                    item.hai_number++
+                    //重新给localStorage设置键值对
+                    localStorage.setItem("cartList4",JSON.stringify(cartList4))
+                }
+            })
+            //判断bool是否为true
+            if(bool){
+                 //修改dt对象中的数量
+                dt.hai_number=1
+                //把当前商品追加到cartList4中
+                cartList4.push(dt)
+                //重新给localStorage设置键值对
+                localStorage.setItem("cartList4",JSON.stringify(cartList4))
+            }
+            location.href='../html/gouwuche.html'
+        }else{
+            //修改dt对象中的数量
+            dt.hai_number=1
+            //把当前商品追加到cartList4中
+            cartList4.push(dt)
+            //重新给localStorage设置键值对
+            localStorage.setItem("cartList4",JSON.stringify(cartList4))
+        }
+    }
+}
